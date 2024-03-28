7ff9d0b2ede7024848164207aa1290cf703e7cc4
Desh Iyer <desh.iyer@gmail.com>
2024-03-28 19:59:20 +0530

    Update: Listen for requests and navigate to the login page after registering

--- server/index.js
+++ server/index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const UserModel = require('./models/User')
+
 const app = express()
 
 // Transfer data from frontend to backend in JSON format
-app.use(express.json)
+app.use(express.json())
 app.use(cors())
 
-// Create MongoDB connection
-mongoose.connect('')
+// Create MongoDB connection to the user database
+mongoose.connect('mongodb://127.0.0.1:27017/user')
+
+// Create the register route
+app.post('/register', (request, result) => {
+    UserModel.create(request.body)
+    .then(users => result.json(users))
+    .catch(err => result.json(err))
+})
+
+app.listen(3001, () => {
+    console.log('Server is running. Listening on port number 3001...')
+})
