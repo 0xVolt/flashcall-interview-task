f17d9efc149c9092d032720532b6406c7dc37396
Desh Iyer <desh.iyer@gmail.com>
2024-03-28 22:37:03 +0530

    Update: Add login route

--- server/index.js
+++ server/index.js
@@ -13,11 +13,30 @@ app.use(cors())
 // Create MongoDB connection to the user database
 mongoose.connect('mongodb://127.0.0.1:27017/user')
 
+// Create the login route
+app.post('/login', (request, response) => {
+    const { email, password } = request.body
+
+    // First check if the user exists then check password
+    UserModel.findOne({ email: email })
+    .then(user => {
+        if (user) {
+            if (user.password === password) {
+                response.json('Success! Logged in.')
+            } else {
+                response.json('Password incorrect!')
+            }
+        } else {
+            response.json('No record of user.')
+        }
+    })
+})
+
 // Create the register route
-app.post('/register', (request, result) => {
+app.post('/register', (request, response) => {
     UserModel.create(request.body)
-    .then(users => result.json(users))
-    .catch(err => result.json(err))
+    .then(users => response.json(users))
+    .catch(err => response.json(err))
 })
 
 app.listen(3001, () => {
