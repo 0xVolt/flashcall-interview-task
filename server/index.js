0c2699d0b3256926bce2ea47a0fcf63f8de667ac
Desh Iyer <desh.iyer@gmail.com>
2024-03-29 12:54:38 +0530

    Update: Default '/' route

--- server/index.js
+++ server/index.js
@@ -8,10 +8,22 @@ const app = express()
 
 // Transfer data from frontend to backend in JSON format
 app.use(express.json())
-app.use(cors())
+
+// Use this to allow cross-origin
+app.use(cors({
+    origin: [''],
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+))
 
 // Create MongoDB connection to the user database
-mongoose.connect('mongodb://127.0.0.1:27017/user')
+// mongoose.connect('mongodb://127.0.0.1:27017/user')
+mongoose.connect('mongodb+srv://deshiyer:CcqajXuZmHXZY1Jv@cluster0.xixrtt2.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0')
+
+app.post('/', (request, response) => {
+  response.json("Hello!")
+})
 
 // Create the login route
 app.post('/login', (request, response) => {
